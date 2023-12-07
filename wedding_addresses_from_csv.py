66aa49061e303382a537d4e041dfddac0046f295
Nathan Perry <dubious90@gmail.com>
2023-12-07 16:59:11 -0500

    Code review feedback.
    - Refactored main logic into functions
    - Some variable renaming
    - Brought in pylint/black
    - Much more commenting
    - Made program more pythonic

--- wedding_addresses_from_csv.py
+++ wedding_addresses_from_csv.py
@@ -1,59 +1,76 @@
 #!/usr/bin/python3
 
+"""Converts a csv of addresses to written envelope addresses."""
+
 import argparse
 import csv
 from functools import partial
+import io
+from typing import Optional
 
-def write_optional_field_as_line(column_positions, output, row, field_name):
-    if (column_positions[field_name]):
-        field = row[column_positions[field_name]]
-        if (field):
-            output.write(field + "\n")
+# _REQUIRED_FIELDS = (name, city, state, zipcode)
+_OPTIONAL_FIELDS = ("guest1", "guest2", "guest3", "guest4", "street1", "street2")
+
+
+def write_optional_field_as_line(
+    column_positions: dict[str, Optional[int]],
+    output_file: io.TextIOWrapper,
+    row: dict,
+    field_name: str,
+) -> None:
+    """If a field exists, writes it as a single line in the output."""
+    if field_name in column_positions:
+        field_value = row[column_positions[field_name]]
+        if field_value:
+            output_file.write(field_value + "\n")
+
+
+def parse_args():
+    """Creates an argument parser for the cli."""
+    parser = argparse.ArgumentParser(
+        "wedding_addresses_from_csv",
+        "Takes in a csv file for a list of addresses and creates a txt file with"
+        + "those addresses in envelope format.",
+        "See README.md for more usage information.",
+    )
+    parser.add_argument("-i", "--input", required=True)
+    parser.add_argument("-o", "--output", default="./envelope_addresses.txt")
+    return parser.parse_args()
+
+
+def parse_column_positions(title_row):
+    """Determine column positions by iterating through the title row."""
+    column_positions = {}
+    for i, header in enumerate(title_row):
+        column_positions[header] = i
+    return column_positions
+
+
+def create_envelope_addresses_from_csv(args):
+    """Reads in a csv input and writes a txt file in envelope format."""
+
+    with open(args.input, newline="", encoding="UTF-8") as input_file, open(
+        args.output, "w", encoding="UTF-8"
+    ) as output:
+        csvreader = csv.reader(input_file)
+        column_positions = parse_column_positions(next(csvreader))
+
+        # Write an address for each row of the csv.
+        for row in csvreader:
+            write_if_exists = partial(
+                write_optional_field_as_line, column_positions, output, row
+            )
+            output.write(row[column_positions["name"]] + "\n")
+            for optional_field in _OPTIONAL_FIELDS:
+                write_if_exists(optional_field)
+
+            city = row[column_positions["city"]]
+            state = row[column_positions["state"]]
+            zipcode = row[column_positions["zipcode"]]
+            output.write(city + ", " + state + " " + zipcode)
+            output.write("\n\n")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        "wedding_addresses_from_csv",
-        "Takes in a csv file for a list of addresses and creates a txt file with those addresses in envelope format.",
-        "See README.md for more usage information.")
-    parser.add_argument("-i", "--input")
-    parser.add_argument("-o", "--output")
-    args = parser.parse_args()
-
-    column_positions = {
-        "name": None,
-        "guest1": None,
-        "guest2": None,
-        "guest3": None,
-        "guest4": None,
-        "street1": None,
-        "street2": None,
-        "city": None,
-        "state": None,
-        "zipcode": None,
-    }
-
-    with open(args.input, newline="") as input, open(args.output, "w", newline="") as output:
-        csvreader = csv.reader(input)
-        title = True
-        for row in csvreader:
-            if title:
-                for i, header in enumerate(row):
-                    column_positions[header] = i
-                title = False
-                print(column_positions)
-            else:
-                write_if_exists = partial(write_optional_field_as_line,column_positions, output, row)
-                output.write(row[column_positions["name"]] + "\n")
-                write_if_exists("guest1")
-                write_if_exists("guest2")
-                write_if_exists("guest3")
-                write_if_exists("guest4")
-                write_if_exists("street1")
-                write_if_exists("street2")
-
-                city = row[column_positions["city"]]
-                state = row[column_positions["state"]]
-                zip = row[column_positions["zipcode"]]
-                output.write(city + ", " + state + " " + zip)
-                output.write("\n\n")
+    parsed_args = parse_args()
+    create_envelope_addresses_from_csv(parsed_args)
